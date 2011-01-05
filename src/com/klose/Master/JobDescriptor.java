607aad47eb3fdb238fcac79b73935721d3a467ed
Bing Jiang <jiangbing@ict.ac.cn>
2011-01-05 16:55:47 +0800

    construct DAG, and need to test it later.

--- src/com/klose/Master/JobDescriptor.java
+++ src/com/klose/Master/JobDescriptor.java
@@ -21,7 +21,7 @@ public class JobDescriptor {
 	private static int taskStatesIndex = 0;// the index of array JobView
 	
 	//HashMap<taskid, taskStatesIndex>
-	private HashMap<String, Integer> JobStatus = new HashMap<String ,Integer>();
+	private HashMap<String, Integer> jobStatus = new HashMap<String ,Integer>();
 	
 	private JobXMLParser parser;
 	
@@ -32,28 +32,50 @@ public class JobDescriptor {
 		loadJobView();
 	}
 	private void loadJobView() {
-		Iterator<Element> taskIter = parser.getTasks().elementIterator();
+		Iterator<Element> taskIter = parser.getTasks();
+		
+		/*In order to generate the suffix dependence between tasks, 
+		store the prefix dependence.For example, task3 depends both 
+		task1 and task2 finished. DAG: 1,2 ->3, store 1:2:3, when 
+		the first structure of DAG has been constructed, it only records
+		3 depends both 1 and 2, it misses the suffix
+		dependence, and later will use the tasksDep accordingly to generate
+		it.In the example above,task 1 and 2 will point to task3.  
+		*/
+		String [] tasksDep = new String[tasksView.length];
+		
 		while(taskIter.hasNext()) {
 			Element taskEle = taskIter.next(); 
 			String taskId = parser.getTaskID(taskEle);
 			int dep = parser.getTaskDep(taskEle);
 			tasksView[taskStatesIndex] = new TaskStates(taskId);
-			JobStatus.put(taskId, taskStatesIndex);
+			jobStatus.put(taskId, taskStatesIndex);
 			if(dep == 0) {
 				tasksView[taskStatesIndex].setStates(TaskState.PREPARED);
 			}
 			else {
 				tasksView[taskStatesIndex].setStates(TaskState.UNPREPARED);
-				Iterator<Element> eleIter = parser.getDepTaskEle(taskEle).elementIterator();
-				while(eleIter.hasNext()) {
-					eleIter.next().
-				}
-				
 			}
+			String prefixDep = parser.getDepTaskEle(taskEle); 
+			if(!prefixDep.equals(""))
+				tasksDep[taskStatesIndex] = new String(prefixDep+":"+taskId);
+			else
+				tasksDep[taskStatesIndex] = new String("");
+			tasksView[taskStatesIndex]
+			          .addPrefixTaskIds(prefixDep);
 			taskStatesIndex ++;
-			
-			
 		}
+		for(int i = 0; i < taskStatesIndex; i++) {
+			if(!tasksDep[i].equals("")) {
+				String [] id = tasksDep[i].split(":");
+				String endId = id[id.length -1];
+				for(int j = 0; j < id.length -1; j++) {
+					tasksView[jobStatus.get(id[j])].addSuffixTaskIds(endId); 
+				}
+			}
+		}
+		
+		
 	}
 	public void parse() {
 		JobXMLParser parser = new JobXMLParser(this.xmlPath);
@@ -69,6 +91,22 @@ public class JobDescriptor {
 		}
 		public void setStates(int state) {
 			this.states = state;
+		}
+		public void addPrefixTaskIds(String taskIds) {
+			if(!taskIds.trim().equals("")) {
+				String [] id = taskIds.split(":");
+				for(String tmp: id) {
+					prefixTaskIds.add(tmp);
+				}
+			}
+		}
+		public void addSuffixTaskIds(String taskIds) {
+			if(!taskIds.trim().equals("")) {
+				String [] id = taskIds.split(":");
+				for(String tmp: id) {
+					suffixTaskIds.add(tmp);
+				}
+			}
 		}
 	}
 }
