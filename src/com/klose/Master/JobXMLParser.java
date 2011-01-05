e17538a57732eb5de1d3f66204990132f0911eca
Bing Jiang <jiangbing@ict.ac.cn>
2011-01-05 16:05:01 +0800

    uniform the transformer xml and code. Test it.

--- src/com/klose/Master/JobXMLParser.java
+++ src/com/klose/Master/JobXMLParser.java
@@ -1,5 +1,7 @@
 package com.klose.Master;
 
+import java.util.Iterator;
+
 import org.dom4j.Element;
 
 import com.klose.common.XMLParser;
@@ -11,52 +13,65 @@ import com.klose.common.XMLParser;
  *
  <?xml version="1.0" encoding="GB2312" standalone="no"?>
 <Job>
-    <id>1_1_201101041945</id>
-    <total>6</total>
-    <task id=task_1_1_1 dep=0/>
-    <task id=task_1_1_2 dep=0/>
-    <task id=task_1_1_3 dep=0/>
-    <task id=task_1_1_4 dep=-2>
-        <taskid>1_1_1</taskid>
-        <taskid>1_1_2</taskid>
-    </task>
-    <task id=1_1_5 dep=-2>
-        <taskid>1_1_2</taskid>
-        <taskid>1_1_3</taskid>
-    </task>
-    <task id=1_1_6 dep=2>
-        <taskid>1_1_4</taskid>
-        <taskid>1_1_5</taskid>
-    </task>
+	<Id>1_1_201101041945</Id>
+	<total>6</total>
+	<task id="1_1_1" dep="0"/>
+	<task id="1_1_2" dep="0"/>
+	<task id="1_1_3" dep="0"/>
+	<task id="1_1_4" dep="-2">
+		<taskid>1_1_1</taskid>
+		<taskid>1_1_2</taskid>
+	</task>
+	<task id="1_1_5" dep="-2">
+		<taskid>1_1_2</taskid>
+		<taskid>1_1_3</taskid>
+	</task>
+	<task id="1_1_6" dep="2">
+		<taskid>1_1_4</taskid>
+		<taskid>1_1_5</taskid>
+	</task>
 </Job>
  *
  */
 public class JobXMLParser extends XMLParser {
 	private final String ID = "Id";
 	private final String Task = "task";
-	private final String TaskDep = "dep";
+	private final String TaskAttriId = "id";
+	private final String TaskAttriDep = "dep";
 	private final String TaskID = "taskid";
 	private final String TaskTotal = "total";
 	
 	public JobXMLParser(String path) {
 		super(path);
 	}
-	public Element getID() {
-		return getRootElement().element(ID);
+	public String getID() {
+		return getRootElement().element(ID).getText();
 	}
 	public int getTaskTotal() {
 		return Integer.parseInt(getRootElement().elementText(TaskTotal));
 	}
-	public Element getTasks() {
-		return getRootElement().element(Task);
+	public Iterator<Element> getTasks() {
+		return getRootElement().elementIterator(Task);
 	}
 	public int getTaskDep(Element taskEle) {
-		return Integer.parseInt(taskEle.attributeValue(TaskDep));
+		return Integer.parseInt(taskEle.attributeValue(TaskAttriDep));
 	}
 	public String getTaskID(Element taskEle) {
-		return taskEle.attributeValue(ID);
+		return taskEle.attributeValue(TaskAttriId);
 	}
-	public Element getDepTaskEle(Element taskEle) {
-		return taskEle.element(TaskID);
+	/**
+	 * return the dependence task id, if it depends more than one task, 
+	 * it will return the taskid:taskid:...:taskid
+	 */
+	public String getDepTaskEle(Element taskEle) {
+		Iterator<Element> iter = taskEle.elementIterator(TaskID);
+		String res = "";
+		if(iter.hasNext()) {
+			res = iter.next().getText();
+			while(iter.hasNext()) {
+				res += (":" + iter.next().getText()); 
+			}
+		}
+		return res;
 	}
 }
--- src/temporary/TestJobXMLParser.java
+++ src/temporary/TestJobXMLParser.java
@@ -3,40 +3,46 @@ package temporary;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
+
+import org.dom4j.Element;
 
 import com.klose.Master.JobXMLParser;
 
 public class TestJobXMLParser {
 	public static void main(String[] args) throws IOException {
 		String content = "" +
-				"<?xml version=\"1.0\" encoding=\"GB2312\" standalone=\"no\"?>\n" +				
-"<Job>\n" +
-    "<Id>1_1_201101041945</Id>\n" +
-    "<total>6</total>\n" +
-    "<task id=\"task_1_1_1\" dep=\"0\"/>\n" +
-    "<task id=\"task_1_1_2\" dep=\"0\"/>\n" +
-    "<task id=\"task_1_1_3\" dep=\"0\"/>\n" +
-    "<task id=\"task_1_1_4\" dep=\"-2\">\n" +
-    "<taskid>1_1_1</taskid>\n" +
-    "<taskid>1_1_2</taskid>\n" +
-    "</task>\n" +
-    "<task id=\"1_1_5\" dep=\"-2\">\n" +
-    "   <taskid>1_1_2</taskid>\n" +
-    "   <taskid>1_1_3</taskid>\n" +
-    "</task>\n" +
-    "<task id=\"1_1_6\" dep=\"2\">\n" +
-    "<taskid>1_1_4</taskid>\n" +
-    "<taskid>1_1_5</taskid>\n" +
-    "</task>\n" +
-    "</Job>\n";
-	File file = new File("/tmp/job.xml");
-	if(!file.exists()) {
-		file.createNewFile();
-	}
-	FileWriter writer = new FileWriter(file);
-	writer.write(content);
-	writer.close();
-	JobXMLParser parser = new JobXMLParser("/tmp/job.xml");
-	System.out.println(parser.getID().getText());	
+		"<?xml version=\"1.0\" encoding=\"GB2312\" standalone=\"no\"?>\n"
+				+ "<Job>\n" + "<Id>1_1_201101041945</Id>\n"
+				+ "<total>6</total>\n"
+				+ "<task id=\"1_1_1\" dep=\"0\"/>\n"
+				+ "<task id=\"1_1_2\" dep=\"0\"/>\n"
+				+ "<task id=\"1_1_3\" dep=\"0\"/>\n"
+				+ "<task id=\"1_1_4\" dep=\"-2\">\n"
+				+ "<taskid>1_1_1</taskid>\n" + "<taskid>1_1_2</taskid>\n"
+				+ "</task>\n" + "<task id=\"1_1_5\" dep=\"-2\">\n"
+				+ "   <taskid>1_1_2</taskid>\n" + "   <taskid>1_1_3</taskid>\n"
+				+ "</task>\n" + "<task id=\"1_1_6\" dep=\"2\">\n"
+				+ "<taskid>1_1_4</taskid>\n" + "<taskid>1_1_5</taskid>\n"
+				+ "</task>\n" + "</Job>\n";
+		File file = new File("/tmp/job.xml");
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter writer = new FileWriter(file);
+		writer.write(content);
+		writer.close();
+		JobXMLParser parser = new JobXMLParser("/tmp/job.xml");
+		System.out.println(parser.getID());
+		System.out.println(parser.getTaskTotal());
+		Iterator<Element> iter = parser.getTasks();
+		while(iter.hasNext()) {
+			System.out.println("***************");
+			Element tmp = iter.next();
+			System.out.println(tmp.asXML());
+			System.out.println(parser.getTaskID(tmp));
+			System.out.println(parser.getTaskDep(tmp));
+			System.out.println(parser.getDepTaskEle(tmp));
+		}
 	}
 }
